0809eed76fbb73b38f4cff634182f5ba8bb2319a
rorisangmadikgetla767-ux <rorisangmadikgetla767@gmail.com>
2026-08-29 09:22:46 +0200

    Another commit

--- main.py
+++ main.py
@@ -73,4 +73,12 @@ def login(credentials: schemas.LoginRequest, db: Session = Depends(get_db)):
             detail="Invalid email / Invalid password"
         )
         
+    if not verify_password(credentials.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email / Invalid password"
+        )
+        
     
+    accessToken = create_access_token(data={"sub": user.email})
+    return {"access_token": accessToken, "token_type": "bearer"}
